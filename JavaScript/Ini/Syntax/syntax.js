b25141de713957faeeb25a2678b8271897ccfc4b
Marc-Antoine Thevenet <mat@thev3.net>
2022-05-20 13:31:32 +0200

    Update syntax.js

--- JavaScript/Ini/Syntax/syntax.js
+++ JavaScript/Ini/Syntax/syntax.js
@@ -273,4 +273,18 @@ function moy() {
 }
 moy(12, 8, 13);
 moy();
+
 // Spread operator
+let notes = [12, 8, 13];
+moy(...notes);
+
+// 20. La récusrsivité
+/*
+Factorielle :
+0! = 1
+1! = 1 * 0! = 1
+2! = 2 * 1! = 2
+3! = 3 * 2! = 6
+4! = 4 * 3! = 24
+n! = n * (n-1)!
+/*
